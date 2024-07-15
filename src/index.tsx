2de75885383b34234bdc69d9981e887a2c1fc408
asukachikaru <asukachikaru@gmail.com>
2024-07-16 00:00:49 +0900

    build: install vite

--- src/index.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
-import "../node_modules/modern-normalize/modern-normalize.css";
-
-ReactDOM.render(<App />, document.getElementById("root"));
