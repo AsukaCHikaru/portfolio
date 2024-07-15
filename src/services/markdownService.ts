ff228ef2d0a492b7134f45bc45431dd84d96300d
asukachikaru <asukachikaru@gmail.com>
2024-07-15 22:09:36 +0900

    feat: blog post frontmatter parser util

--- src/services/markdownService.ts
+++ src/services/markdownService.ts
@@ -1,6 +1,8 @@
+import type { BlogPostMeta } from "../types";
+import { parseBlogPostMeta } from "../utils/markdownUtils";
 import { request } from "./apiCore";
 
-export const getBlogList = async () => {
+export const getBlogList = async (): Promise<BlogPostMeta[]> => {
   const data = await request("/api/blogList");
-  return data;
+  return data.map(parseBlogPostMeta);
 };
---
+++ src/utils/markdownUtils.ts
@@ -0,0 +1,15 @@
+import type { BlogPostLanguage, BlogPostMeta } from "../types";
+
+export const parseBlogPostMeta = (
+  input: Record<string, string>,
+): BlogPostMeta => {
+  return {
+    category: input.category,
+    title: input.title,
+    pathname: input.pathname,
+    description: input.description,
+    published: input.published,
+    updated: input.updated || input.published,
+    language: input.language as BlogPostLanguage,
+  };
+};
