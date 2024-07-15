01b4ee5b37ccb11ac94debbc4814daae62dc0b4f
asukachikaru <asukachikaru@gmail.com>
2024-07-15 22:09:42 +0900

    feat: useApi hook

--- src/services/apiCore.ts
+++ src/services/apiCore.ts
@@ -1,4 +1,19 @@
+import { useEffect, useState } from "react";
+
 export const request = async (pathname: string) => {
   const response = await fetch("http://localhost:3000" + pathname);
   return response.json();
+};
+
+export const useApi = <T>(api: () => Promise<T>) => {
+  const [data, setData] = useState<T>();
+
+  useEffect(() => {
+    if (data !== undefined) {
+      return;
+    }
+    api().then((res) => setData(res));
+  }, [data]);
+
+  return data;
 };
