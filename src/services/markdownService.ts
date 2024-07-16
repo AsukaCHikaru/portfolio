9efe269e61819ccec34ae177de8f6a15a28ce783
asukachikaru <asukachikaru@gmail.com>
2024-07-16 22:50:00 +0900

    feat: getBlogPost type

--- src/services/markdownService.ts
+++ src/services/markdownService.ts
@@ -1,4 +1,5 @@
 import type { BlogPostMeta } from "../../types";
+import type { MarkdownBlock } from "../../types/markdown";
 import { parseBlogPostMeta } from "../utils/markdownUtils";
 import { request } from "./apiCore";
 
@@ -7,7 +8,10 @@ export const getBlogPostList = async (): Promise<BlogPostMeta[]> => {
   return data.map(parseBlogPostMeta);
 };
 
-export const getBlogPost = async (postPath: string): Promise<unknown> => {
+export const getBlogPost = async (
+  postPath: string,
+): Promise<{ content: MarkdownBlock[] }> => {
   const data = await request(`/api/blog/${postPath}`);
+  console.log(data);
   return data;
 };
