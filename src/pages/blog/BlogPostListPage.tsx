6b0e7b944aa74ca215fd3391c820db977de4aec1
asukachikaru <asukachikaru@gmail.com>
2024-07-17 09:20:20 +0900

    feat: archive post link

--- src/pages/blog/BlogPostListPage.tsx
+++ src/pages/blog/BlogPostListPage.tsx
@@ -3,6 +3,7 @@ import type { BlogPostMeta } from "../../../types";
 import { useApi } from "../../services/apiCore";
 import { getBlogPostList } from "../../services/markdownService";
 import styles from "./BlogPostListPage.module.css";
+import { Link } from "@tanstack/react-router";
 
 export const BlogPostListPage = () => {
   const blogPostList = useApi({
@@ -24,5 +25,11 @@ interface BlogPostLinkProps {
   post: BlogPostMeta;
 }
 const BlogPostLink: FC<BlogPostLinkProps> = ({ post }) => (
-  <a href={`/blog/${post.pathname}`}>{post.title}</a>
+  <Link to={`/blog/${post.pathname}`} className={styles["link-wrapper"]}>
+    <h2 className={styles["post-title"]}>{post.title}</h2>
+    {post.description ? (
+      <p className={styles["post-description"]}>{post.description}</p>
+    ) : null}
+    <div className={styles["post-date"]}>{post.published}</div>
+  </Link>
 );
