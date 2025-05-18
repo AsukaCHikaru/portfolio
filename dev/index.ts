2abb2ffc199386b46a8f544422f84dba3d4b3af6
asukachikaru <asuka@asukawang.com>
2025-05-18 12:14:00 +0900

    chore: remove old files

--- dev/index.ts
+++
@@ -1,7 +0,0 @@
-import { bootServer, resolveMarkdownFile } from "./devServer";
-
-const FILE_PORT = 3001;
-
-bootServer(FILE_PORT, resolveMarkdownFile);
-
-Bun.spawn(["vite"], { stdout: "inherit" });
