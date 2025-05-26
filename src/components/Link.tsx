63344e06bfbf679f2ea5816465400b73794a5b4d
asukachikaru <asuka@asukawang.com>
2025-05-26 23:11:16 +0900

    chore: remove link log

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -2,7 +2,6 @@ import type { ReactNode } from "react";
 
 export const Link = ({ children, to }: { children: ReactNode; to: string }) => {
   const handleClick = (event: React.MouseEvent<HTMLAnchorElement>) => {
-    console.log("Link clicked:", to);
     event.preventDefault();
     window.history.pushState({}, "", to);
     window.dispatchEvent(
