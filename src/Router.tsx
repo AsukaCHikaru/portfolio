3cba4de477fc4dc7c52620c18c225389ed2845d4
asukachikaru <asuka@asukawang.com>
2025-05-26 22:29:25 +0900

    refactor: router

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,32 +1,9 @@
-import { useState, useEffect, useCallback } from "react";
+import { useState, useEffect } from "react";
 import { AboutPage } from "./pages/about/AboutPage";
 import { ArchivePage } from "./pages/blog/ArchivePage";
 import { PostPage } from "./pages/blog/PostPage";
 import { FrontPage } from "./pages/frontpage/FrontPage";
 import { ResumePage } from "./pages/resume/ResumePage";
-
-const routes = [
-  {
-    path: "/blog/:slug",
-    component: PostPage,
-  },
-  {
-    path: "/blog",
-    component: ArchivePage,
-  },
-  {
-    path: "/about",
-    component: AboutPage,
-  },
-  {
-    path: "/resume",
-    component: ResumePage,
-  },
-  {
-    path: "/",
-    component: FrontPage,
-  },
-];
 
 export const Router = () => {
   const [path, setPath] = useState(window.location.pathname);
@@ -43,15 +20,22 @@ export const Router = () => {
     };
   }, []);
 
-  const renderContent = useCallback(() => {
-    for (const route of routes) {
-      const urlPattern = new RegExp(route.path.replace(/\/:\w+/, "\/\\S+"));
-      if (urlPattern.test(path)) {
-        return route.component();
-      }
+  return (() => {
+    if (/^\/$/.test(path)) {
+      return <FrontPage />;
+    }
+    if (/^\/blog\/[^/]+\/?$/.test(path)) {
+      return <PostPage />;
+    }
+    if (/^\/blog\/?$/.test(path)) {
+      return <ArchivePage />;
+    }
+    if (/^\/about\/?$/.test(path)) {
+      return <AboutPage />;
+    }
+    if (/^\/resume\/?$/.test(path)) {
+      return <ResumePage />;
     }
     return <div>404</div>;
-  }, [path]);
-
-  return renderContent();
+  })();
 };
