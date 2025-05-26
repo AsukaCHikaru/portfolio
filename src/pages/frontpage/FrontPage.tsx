b0093c9e58cc452bde9d21ace6dd0e5b957f3256
asukachikaru <asuka@asukawang.com>
2025-05-26 23:11:02 +0900

    fix: use data context in frontpage

--- src/pages/frontpage/FrontPage.tsx
+++ src/pages/frontpage/FrontPage.tsx
@@ -1,6 +1,19 @@
+import { useContext } from "react";
+import { DataContext } from "../../components/DataContext";
 import { PostPageContent } from "../../components/PostPageContent";
 
 export const FrontPage = () => {
-  const post = window.__STATIC_PROPS__.post;
+  const context = useContext(DataContext);
+  const post =
+    window.__STATIC_PROPS__.post ||
+    context.postList.find(
+      (post) =>
+        post.metadata.pathname === window.location.pathname.split("/").pop(),
+    );
+
+  if (!post) {
+    return null;
+  }
+
   return <PostPageContent metadata={post.metadata} content={post.content} />;
 };
