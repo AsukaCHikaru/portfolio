4f4d1436f48446e77735dfe663ac98dde47b1d4c
asukachikaru <asuka@asukawang.com>
2025-06-01 22:45:57 +0900

    feat: front page header

--- src/pages/frontpage/FrontPage.tsx
+++ src/pages/frontpage/FrontPage.tsx
@@ -1,6 +1,9 @@
 import { useContext, useMemo } from "react";
 import { DataContext } from "../../components/DataContext";
+import type { PostMetaData } from "../../types";
+import type { Block } from "@asukawang/amp";
 import { PostPageContent } from "../../components/PostPageContent";
+import { Link } from "../../components/Link";
 
 export const FrontPage = () => {
   const context = useContext(DataContext);
@@ -18,5 +21,62 @@ export const FrontPage = () => {
     return null;
   }
 
-  return <PostPageContent metadata={post.metadata} content={post.content} />;
+  return (
+    <FrontPageContent
+      leadStory={{
+        metadata: post.metadata,
+        content: post.content,
+      }}
+      lastUpdated={post.metadata.publishedAt}
+    />
+  );
 };
+
+interface Props {
+  leadStory: { metadata: PostMetaData; content: Block[] };
+  lastUpdated: string;
+}
+
+export const FrontPageContent = ({ leadStory, lastUpdated }: Props) => {
+  return (
+    <div className="site_container">
+      <Header lastUpdated={lastUpdated} />
+      <LeadStory leadStory={leadStory} />
+    </div>
+  );
+};
+
+const Header = ({ lastUpdated }: { lastUpdated: string }) => (
+  <header className="frontpage-header">
+    <div>
+      <div className="frontpage-header_slogan">
+        <p>"The Work{"\n"}Goes On"</p>
+      </div>
+      <h1>
+        <a href="/">ASUKA WANG</a>
+      </h1>
+      <div>
+        Last Updated{"\n"}
+        <time dateTime={lastUpdated}>{lastUpdated}</time>
+      </div>
+    </div>
+    <nav>
+      <Link to="/blog">blog</Link>
+      <Link to="/about">about</Link>
+      <Link to="/rss">rss</Link>
+    </nav>
+  </header>
+);
+
+const LeadStory = ({
+  leadStory,
+}: {
+  leadStory: { metadata: PostMetaData; content: Block[] };
+}) => (
+  <article className="fontpage-lead-story_container">
+    <PostPageContent
+      metadata={leadStory.metadata}
+      content={leadStory.content}
+    />
+  </article>
+);
