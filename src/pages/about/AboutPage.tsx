ab6ea31fe758f96ff779abc9c8292f2800b9223a
asukachikaru <asuka@asukawang.com>
2025-06-08 16:45:58 +0900

    feat: about page style

--- src/pages/about/AboutPage.tsx
+++ src/pages/about/AboutPage.tsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { DataContext } from "../../components/DataContext";
-import { PostPageContent } from "../../components/PostPageContent";
+import { Layout } from "../../components/Layout";
+import { ContentBlock } from "../../components/ContentBlock";
 
 export const AboutPage = () => {
   const context = useContext(DataContext);
@@ -10,5 +11,14 @@ export const AboutPage = () => {
     return null;
   }
 
-  return <PostPageContent metadata={post.metadata} content={post.content} />;
+  return (
+    <Layout>
+      <h1 className="about-page-header">About</h1>
+      <article className="post-page-content grid">
+        {post.content.map((block, i) => (
+          <ContentBlock key={i} block={block} />
+        ))}
+      </article>
+    </Layout>
+  );
 };
