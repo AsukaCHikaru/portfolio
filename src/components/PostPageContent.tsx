da89d380d091ce1ca71270ec8b01e18d6c8bc6d3
asukachikaru <asuka@asukawang.com>
2025-09-08 00:09:36 +0900

    feat: post category link in header

--- src/components/PostPageContent.tsx
+++ src/components/PostPageContent.tsx
@@ -3,6 +3,7 @@ import { Layout } from "./Layout";
 import type { PostMetaData } from "../types";
 import { ContentBlock } from "./ContentBlock";
 import { formatDate } from "../utils/dateTimeUtil";
+import { Link } from "./Link";
 
 interface Props {
   metadata: PostMetaData;
@@ -12,11 +13,12 @@ export const PostPageContent = ({ metadata, content }: Props) => {
   return (
     <Layout>
       <div className="post-page-header_container">
+        <Link to={`/blog/?category=${metadata.category}`}>
+          {metadata.category}
+        </Link>
         <h1>{metadata.title}</h1>
         <h2>{metadata.description}</h2>
-        <p>
-          {formatDate(metadata.publishedAt)}
-        </p>
+        <p>{formatDate(metadata.publishedAt)}</p>
       </div>
       <article className="post-page-content grid">
         {content.map((block, i) => (
