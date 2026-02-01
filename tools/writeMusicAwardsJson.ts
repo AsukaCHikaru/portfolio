69f3ac352edde1b0288945d594c2f36a3bb6a4e7
asukachikaru <asuka@asukawang.com>
2026-02-01 16:36:51 +0900

    refactor: write music awards json

---
+++ tools/writeMusicAwardsJson.ts
@@ -0,0 +1,133 @@
+import { existsSync, readdirSync, readFileSync } from "fs";
+import { writeFile } from "fs/promises";
+
+const musicAwardsFolderPath = "./symbolicLinks/list/musicAwards";
+
+const checkSymlinkExist = (path: string) => {
+  if (existsSync(path)) {
+    return;
+  }
+  process.exit(0);
+};
+
+const getFileList = (folderPath: string) => {
+  const years = readdirSync(folderPath).filter((file) =>
+    /Music Awards \d{4}\.md$/.test(file),
+  );
+  return years;
+};
+
+const sortYearDescending = (a: string, b: string) => {
+  const yearA = a.match(/\d{4}/)?.[0];
+  const yearB = b.match(/\d{4}/)?.[0];
+  if (yearA === undefined || yearB === undefined) {
+    return 0;
+  }
+  return parseInt(yearB) - parseInt(yearA);
+};
+
+type RecordNominee = {
+  title: string;
+  artist: string;
+  feat?: string;
+  isWinner: boolean;
+};
+type ArtistNominee = {
+  artist: string;
+  isWinner: boolean;
+};
+type Nominee = RecordNominee | ArtistNominee;
+
+const mapLineToNominee = (items: string[], category: string): Nominee => {
+  const isWinner = items[0] === "O";
+  const trimmed = isWinner ? items.slice(2) : items;
+
+  return category.includes("Artist")
+    ? {
+        artist: trimmed[0],
+        isWinner,
+      }
+    : {
+        title: trimmed[0],
+        artist: trimmed[1],
+        feat: trimmed[2] || undefined,
+        isWinner,
+      };
+};
+
+const groupByCategory = (items: string[][]) => {
+  const grouped: string[][][] = [];
+  items.forEach((item) => {
+    if (item[0] === "O") {
+      grouped.push([item]);
+    } else {
+      grouped[grouped.length - 1].push(item);
+    }
+  });
+
+  const categories: Record<string, Nominee[]> = {};
+  grouped.forEach((group) => {
+    const category = group[0][1];
+    group.forEach((item) => {
+      if (item[0] === "O") {
+        categories[category] = [mapLineToNominee(item, category)];
+      } else {
+        categories[category].push(mapLineToNominee(item, category));
+      }
+    });
+  });
+
+  return categories;
+};
+
+const parseTable = (table: string) => {
+  const lines = table
+    .split("\n")
+    .filter((line) => line.startsWith("|"))
+    .slice(2)
+    .map(parseTableLine);
+
+  const parsed = groupByCategory(lines);
+  return parsed;
+};
+
+const parseTableLine = (line: string) =>
+  line
+    .replace(/\[\[([\s\S]+?)\]\]/g, "$1") // trim obsidian link brackets
+    .split("|")
+    .map((cell) => cell.trim())
+    .filter((cell) => cell !== "");
+
+const run = async () => {
+  checkSymlinkExist(musicAwardsFolderPath);
+
+  const files = getFileList(musicAwardsFolderPath);
+  const sortedFiles = files.sort(sortYearDescending);
+
+  const yearMap = new Map<string, Record<string, Nominee[]>>();
+  sortedFiles.forEach((file) => {
+    const content = readFileSync(`${musicAwardsFolderPath}/${file}`, "utf8");
+    const table = content.match(/#\sList\n[^#]+/)?.[0];
+    if (!table) {
+      throw new Error(`No table found in ${file}`);
+    }
+    const parsed = parseTable(table);
+    const year = file.match(/\d{4}/)?.[0];
+    if (!year) {
+      throw new Error(`No year found in ${file}`);
+    }
+    yearMap.set(year, parsed);
+  });
+
+  await writeFile(
+    "./public/contents/list/musicAwards.json",
+    JSON.stringify(Object.fromEntries(yearMap), null, 2),
+  );
+  console.log(
+    `Music awards markdown data written to JSON file ${"./public/contents/list/musicAwards.json"}`,
+  );
+};
+
+if (import.meta.main) {
+  run();
+}
